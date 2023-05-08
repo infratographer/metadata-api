a4b2935684dd236a4c35f72aa0389f408eed411c
Nicole Hubbard <code@nicole.dev>
2023-05-08 11:29:14 -0500

    Update cmd/errors.go
    Co-authored-by: Sarah Funkhouser <11655165+sfunkhouser@users.noreply.github.com>
    Signed-off-by: Nicole Hubbard <code@nicole.dev>

--- cmd/errors.go
+++ cmd/errors.go
@@ -3,6 +3,6 @@ package cmd
 import "errors"
 
 var (
-	// ErrAuditFilePathRequired is returned when a audit file path is missing
+	// ErrAuditFilePathRequired is returned when an audit file path is missing
 	ErrAuditFilePathRequired = errors.New("audit file path is required and cannot be empty")
 )
